a53fabf0db005aa952b4088f28a623bedb0a2f49
Tommaso-Lippolis <Tommaso.Lippolis1@it.ey.com>
2025-08-19 20:51:36 +0200

    feat(black): testato il formatter automatico

--- Lezione_1/esercizio_1/text_analysis.py
+++ Lezione_1/esercizio_1/text_analysis.py
@@ -1,7 +1,6 @@
-
 def read_file(filename):
     try:
-        with open(filename, 'r', encoding='utf-8') as file:
+        with open(filename, "r", encoding="utf-8") as file:
             return "file loaded correctly"
     except FileNotFoundError:
         print(f"File {filename} not found.")
@@ -9,7 +8,7 @@ def read_file(filename):
 
 
 def count_rows(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, "r", encoding="utf-8") as file:
         count = 0
         for line in file:
             count += 1
@@ -17,16 +16,16 @@ def count_rows(filename):
 
 
 def count_words(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, "r", encoding="utf-8") as file:
         count = 0
         for line in file:
             words = line.split()
             count += len(words)
         return count
-    
+
 
 def top_5_occurrences(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, "r", encoding="utf-8") as file:
         word_count = {}
         for line in file:
             words = line.split()
@@ -36,25 +35,26 @@ def top_5_occurrences(filename):
                         word_count[word] += 1
                     else:
                         word_count[word] = 1
-        sorted_words = sorted(word_count.items(), key=lambda item: item[1], reverse=True)
+        sorted_words = sorted(
+            word_count.items(), key=lambda item: item[1], reverse=True
+        )
         return sorted_words[:5]
 
 
-file_path = '18_agosto/esercizio_1/prova.txt'
+file_path = "Lezione_1/esercizio_1/prova.txt"
 
 print(read_file(file_path))
-rows=count_rows(file_path)
-words=count_words(file_path)
-top_5=top_5_occurrences(file_path)
+rows = count_rows(file_path)
+words = count_words(file_path)
+top_5 = top_5_occurrences(file_path)
 
 print(f"Number of rows: {rows}")
 print(f"Number of words: {words}")
 print("Top 5 occurrences:", top_5)
 
-with open('18_agosto/esercizio_1/output.txt', 'w', encoding='utf-8') as output_file:
+with open("Lezione_1/esercizio_1/output.txt", "w", encoding="utf-8") as output_file:
     output_file.write(f"Number of rows: {rows}\n")
     output_file.write(f"Number of words: {words}\n")
     output_file.write("Top 5 occurrences:\n")
     for word, count in top_5:
         output_file.write(f"{word}: {count}\n")
-
