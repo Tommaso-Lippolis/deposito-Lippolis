1effedaab102ede022d824c14e23ddf0650886f7
Tommaso-Lippolis <Tommaso.Lippolis1@it.ey.com>
2025-08-19 10:51:15 +0200

    feat(classificatore) : aggiunti import

---
+++ 19_agosto/esercizio_1/classificatore.py
@@ -0,0 +1,13 @@
+# #### Setup iniziale
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+
+
+
+
+
+
