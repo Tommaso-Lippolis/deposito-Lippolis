fdcaece4a4f7aaa20a108decfd67f7787218cc15
Tommaso-Lippolis <Tommaso.Lippolis1@it.ey.com>
2025-08-19 20:34:12 +0200

    testata variabile __name__ conn modulo importato

--- Lezione_1/prova.py
+++ Lezione_1/prova.py
@@ -1,3 +1,3 @@
-from prova import main
+from prova.main import main
 
 main()
--- Lezione_1/prova/main.py
+++ Lezione_1/prova/main.py
@@ -2,6 +2,7 @@ def main():
     print("Hello, World!")
 
 if __name__ == "__main__":
+    print(f'this script is being run directly: {__name__}')
     main()
 else:
-    print("This script is being imported, not run directly.")
+    print("This script is being imported, not run directly:", __name__)
