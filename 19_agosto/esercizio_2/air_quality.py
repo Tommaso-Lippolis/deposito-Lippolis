62fff3e14a7878052b93417fd53d3e008f16737f
Tommaso-Lippolis <Tommaso.Lippolis1@it.ey.com>
2025-08-19 16:41:07 +0200

    feat(logreg): implememtato scaling e previsioni con log reg

--- 19_agosto/esercizio_2/air_quality.py
+++ 19_agosto/esercizio_2/air_quality.py
@@ -24,6 +24,8 @@ print(df.describe())
 # lasciamo solo le colonne  'Date', 'Time', 'CO(GT)'
 df = df[['Date', 'Time', 'CO(GT)']]
 
+print(df.columns)
+
 df = df.dropna()
 
 # vediamo seci sono valori nulli
@@ -36,15 +38,34 @@ df["CO(GT)"] = pd.to_numeric(df["CO(GT)"].astype(str).str.replace(',', '.'), err
 # identifichiamo come buona qualita dell aria o scarsa qualita dell'aria in base alla media globale
 df['AirQuality'] = (df['CO(GT)'] < df['CO(GT)'].mean()).astype(int)
 
+print("Distribuzione della qualità dell'aria:")
+print(df['AirQuality'].value_counts())
 
-# come input valore inquinante CO e la data il resto cancelliamo
-X = df[['CO(GT)', 'Date', 'Time']]
+
+# come input valore inquinante CO e l ora il resto droppiamo
+df['Hour'] = pd.to_datetime(df['Time'], format='%H.%M.%S', errors='coerce').dt.hour
+X = df[['Hour']]
 
 y = df['AirQuality']
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, stratify=y, test_size=0.25, random_state=42
 )
+
+# Scaling
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
+# Logistic Regression
+log_reg = LogisticRegression(random_state=42, max_iter=1000, class_weight='balanced')
+log_reg.fit(X_train, y_train)
+y_pred_log = log_reg.predict(X_test)
+print("Logistic Regression Classifier Report:")
+print(classification_report(y_test, y_pred_log))
+
+
 
 
 
