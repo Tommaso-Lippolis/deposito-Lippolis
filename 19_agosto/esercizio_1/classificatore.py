87eb658a0d24b6164f240d6005feffb8ad5db6d1
Tommaso-Lippolis <Tommaso.Lippolis1@it.ey.com>
2025-08-19 10:54:00 +0200

    feat(modello): creazione variabile target

--- 19_agosto/esercizio_1/classificatore.py
+++ 19_agosto/esercizio_1/classificatore.py
@@ -6,6 +6,17 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report
 
+# #### Caricamento del dataset
+# Kaggle "AEP_hourly": columns => ["Datetime", "AEP_MW"]
+df = pd.read_csv("AEP_hourly.csv", parse_dates=["Datetime"])
+
+# #### Preprocessing
+df["Hour"] = df["Datetime"].dt.hour
+df["Day"] = df["Datetime"].dt.dayofweek
+df["Month"] = df["Datetime"].dt.month
+
+# Creazione della variabile target: 1 se AEP_MW è maggiore della media, altrimenti 0
+df["Target"] = (df["AEP_MW"] > df["AEP_MW"].mean()).astype(int)
 
 
 
